dc3f95df8f8ac7dd9509a1ab1ddbbc30186fb177
manhnvan <manh12344444444@gmail.com>
2020-12-19 10:38:17 +0700

    fake data

--- house-rental-frontend/src/Components/Chatbox/Chatbox.js
+++ house-rental-frontend/src/Components/Chatbox/Chatbox.js
@@ -47,32 +47,139 @@ function Chatbox({ location }) {
 		});
 	}, []);
 
+	console.log(messagesList)
+
 	useEffect(
 		() => {
-			if (chatboxId !== '') {
-				console.log('calling');
-				socket.emit('join', { name, chatboxId }, () => { });
-				axios
-					.get('http://localhost:3002/getChatbox/' + chatboxId)
-					.then((result) => {
-						setMessagesList(result.data.messages);
-					})
-					.catch((error) => console.log(error));
-			}
+			// if (chatboxId !== '') {
+			// 	console.log('calling');
+			// 	socket.emit('join', { name, chatboxId }, () => { });
+			// 	axios
+			// 		.get('http://localhost:3002/getChatbox/' + chatboxId)
+			// 		.then((result) => {
+			// 			setMessagesList(result.data.messages);
+			// 		})
+			// 		.catch((error) => console.log(error));
+			// }
+			const message = [
+				{
+					"_id": "5fca1b26060b33363cc82800",
+					"senderId": "5fc60d1ffc13ae7947000000",
+					"senderName": "Micheal",
+					"content": "jhkhdkfha"
+				},
+				{
+					"_id": "5fca1b29060b33363cc82801",
+					"senderId": "5fc60d1ffc13ae7947000000",
+					"senderName": "Micheal",
+					"content": "jhkhdkfhafdalhfdl"
+				},
+				{
+					"_id": "5fca1b2a060b33363cc82802",
+					"senderId": "5fc60d1ffc13ae7947000000",
+					"senderName": "Micheal",
+					"content": "jhkhdkfhafdalhfdldfkahsdf"
+				},
+				{
+					"_id": "5fca1b2c060b33363cc82803",
+					"senderId": "5fc60d1ffc13ae7947000000",
+					"senderName": "Micheal",
+					"content": "jhkhdkfhafdalhfdldfkahsdf"
+				},
+				{
+					"_id": "5fca1b2c060b33363cc82804",
+					"senderId": "5fc60d1ffc13ae7947000000",
+					"senderName": "Micheal",
+					"content": "jhkhdkfhafdalhfdldfkahsdf"
+				},
+				{
+					"_id": "5fca1b2c060b33363cc82805",
+					"senderId": "5fc60d1ffc13ae7947000000",
+					"senderName": "Micheal",
+					"content": "jhkhdkfhafdalhfdldfkahsdf"
+				},
+				{
+					"_id": "5fca1b6b060b33363cc82806",
+					"senderId": "5fc60d1ffc13ae7947000000",
+					"senderName": "Manh",
+					"content": "đây là Minh"
+				},
+				{
+					"_id": "5fca1f2d060b33363cc82807",
+					"senderId": "5fc60d1ffc13ae7947000001",
+					"senderName": "Nam",
+					"content": "Nhung ơi"
+				},
+				{
+					"_id": "5fca1f35060b33363cc82808",
+					"senderId": "5fc60d1ffc13ae7947000000",
+					"senderName": "Manh",
+					"content": "ơi"
+				},
+				{
+					"_id": "5fd1d031cfd76c251cf1cca8",
+					"senderId": "5fc60d1ffc13ae7947000001",
+					"senderName": "Nam",
+					"content": "Đại ơi"
+				},
+				{
+					"_id": "5fd8726a0185eb089c1ef3dd",
+					"senderId": "5fc60d1ffc13ae7947000001",
+					"senderName": "Nam",
+					"content": "Name"
+				},
+				{
+					"_id": "5fd8727f0185eb089c1ef3de",
+					"senderId": "5fc60d1ffc13ae7947000000",
+					"senderName": "Micheal",
+					"content": "Minh làm cái này là ổn"
+				},
+				{
+					"_id": "5fd8733e0185eb089c1ef3df",
+					"senderId": "5fc60d1ffc13ae7947000000",
+					"senderName": "Micheal",
+					"content": "Minh làm cái này là ổn"
+				},
+				{
+					"_id": "5fd8733e0185eb089c1ef3e0",
+					"senderId": "5fc60d1ffc13ae7947000000",
+					"senderName": "Micheal",
+					"content": "Minh làm cái này là ổn"
+				},
+				{
+					"_id": "5fd8733e0185eb089c1ef3e1",
+					"senderId": "5fc60d1ffc13ae7947000000",
+					"senderName": "Micheal",
+					"content": "Minh làm cái này là ổn"
+				},
+				{
+					"_id": "5fd8733f0185eb089c1ef3e2",
+					"senderId": "5fc60d1ffc13ae7947000000",
+					"senderName": "Micheal",
+					"content": "Minh làm cái này là ổn"
+				},
+				{
+					"_id": "5fdd71cf71cc1a5288432ef4",
+					"senderId": "5fc60d1ffc13ae7947000000",
+					"senderName": "Micheal",
+					"content": "Cái gì đây"
+				}
+			]
+			setMessagesList(message)
 		},
 		[chatboxId]
 	);
 
-	const sendMessage = (event) => {
-		event.preventDefault();
-		if (message) {
-			socket.emit(
-				'sendMessage',
-				{ content: message, senderId: userId, chatboxId: chatboxId, senderName: name },
-				() => setMessage('')
-			);
-		}
-	};
+	// const sendMessage = (event) => {
+	// 	event.preventDefault();
+	// 	if (message) {
+	// 		socket.emit(
+	// 			'sendMessage',
+	// 			{ content: message, senderId: userId, chatboxId: chatboxId, senderName: name },
+	// 			() => setMessage('')
+	// 		);
+	// 	}
+	// };
 
 	return (
 		<div className="">
@@ -97,7 +204,7 @@ function Chatbox({ location }) {
 							message={message}
 							messages={messagesList}
 							setMessage={setMessage}
-							sendMessage={sendMessage}
+						// sendMessage={sendMessage}
 						/>
 					</div>
 				</div>
