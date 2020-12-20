a15e399cae8a9b4160bd60b35b1185a93ffd20e8
nhungkaylee <56378286+nhungkaylee@users.noreply.github.com>
2020-12-20 11:23:19 +0700

    Update Modal

--- house-rental-frontend/src/Components/LoginPage/index.jsx
+++ house-rental-frontend/src/Components/LoginPage/index.jsx
@@ -27,50 +27,61 @@ function LoginPage() {
     <>
       <button onClick={openModal}>User: CLick to Sign in</button>
       <Modal open={open} onClose={closeModal} center>
-        <div
-          class={`sign-in-box ${isSwitch ? "right-panel-active" : ""}`}
-          id="sign-in-box"
-        >
-          <Login></Login>
-          <Register></Register>
-          <div class="overlay-container">
-            <div class="overlay">
-              <div class="overlay-panel overlay-left">
-                <h1>Welcome Back!</h1>
-                <p>
-                  To keep connected with us please login with your personal info
-                </p>
-                <button class="ghost" id="signIn" onClick={onSignInButtonClick}>
-                  Sign In
-                </button>
-              </div>
-              <div class="overlay-panel overlay-right">
-                <h1>Hello, Friend!</h1>
-                <p>Enter your personal details and start journey with us</p>
-                <button class="ghost" id="signUp" onClick={onSignUpButtonClick}>
-                  Sign Up
-                </button>
+        <div id="sign-in-box">
+          <div
+            class={`sign-in-box ${isSwitch ? "right-panel-active" : ""}`}
+            id="sign-in-box"
+          >
+            <Login></Login>
+            <Register></Register>
+            <div class="overlay-container">
+              <div class="overlay">
+                <div class="overlay-panel overlay-left">
+                  <h1>Welcome Back!</h1>
+                  <p>
+                    To keep connected with us please login with your personal
+                    info
+                  </p>
+                  <button
+                    class="ghost"
+                    id="signIn"
+                    onClick={onSignInButtonClick}
+                  >
+                    Sign In
+                  </button>
+                </div>
+                <div class="overlay-panel overlay-right">
+                  <h1>Hello, Friend!</h1>
+                  <p>Enter your personal details and start journey with us</p>
+                  <button
+                    class="ghost"
+                    id="signUp"
+                    onClick={onSignUpButtonClick}
+                  >
+                    Sign Up
+                  </button>
+                </div>
               </div>
             </div>
           </div>
-        </div>
 
-        <footer>
-          <p>
-            Created with <i class="fa fa-heart"></i> by
-            <a target="_blank" href="https://florin-pop.com">
-              Florin Pop
-            </a>
-            - Read how I created this and how you can join the challenge
-            <a
-              target="_blank"
-              href="https://www.florin-pop.com/blog/2019/03/double-slider-sign-in-up-form/"
-            >
-              here
-            </a>
-            .
-          </p>
-        </footer>
+          <footer>
+            <p>
+              Created with <i class="fa fa-heart"></i> by
+              <a target="_blank" href="https://florin-pop.com">
+                Florin Pop
+              </a>
+              - Read how I created this and how you can join the challenge
+              <a
+                target="_blank"
+                href="https://www.florin-pop.com/blog/2019/03/double-slider-sign-in-up-form/"
+              >
+                here
+              </a>
+              .
+            </p>
+          </footer>
+        </div>
       </Modal>
     </>
   );
