30ceb2a985799daa8a30cc5c89446376c3cbc752
RyeNguyen <48546888+RyeNguyen@users.noreply.github.com>
2020-12-24 00:11:23 +0700

    Styling statistic data for admin

--- house-rental-frontend/src/Components/AdminPageComponent/AdminOwnerAccountManagement/AdminStatisticTabs.js
+++ house-rental-frontend/src/Components/AdminPageComponent/AdminOwnerAccountManagement/AdminStatisticTabs.js
@@ -3,11 +3,129 @@ import { apiGetPoster } from '../../../Services/accommodation_poster_services'
 import moment from 'moment'
 import _ from 'lodash'
 import { posterStatisticDataCoverter } from '../../../Helper/satisticDataTransform'
-
+import { Statistic, Tooltip } from 'antd';
+import './AdminStatisticTable.css';
 
 const MAX_NUMBER_RECORD = 5
 
 function AdminStatisticTabs() {
+
+    //Fake Data
+    const ratePerWeek = [
+        {
+            "title": "Biệt thự ven Hồ phường 3 quận Phú Nhuận",
+            "address": "số 8 ngõ 95/58/12",
+            "subDistrict": "Phúc Xá",
+            "district": "Ba Đình",
+            "city": "Hà Nội",
+            "rate": 4.5,
+            "statisticNumber": 10,
+            "posterId": "5fe0ade609158b4958175dd1",
+            "ratingId": "5fe0ade609158b4958175dd0"
+        },
+        {
+            "title": "Biệt thự ven Hồ phường 3 quận Phú Nhuận",
+            "address": "số 8 ngõ 95/58/12",
+            "subDistrict": "Phúc Xá",
+            "district": "Ba Đình",
+            "city": "Hà Nội",
+            "rate": 0,
+            "statisticNumber": 3,
+            "posterId": "5fe0addb09158b4958175dc5",
+            "ratingId": "5fe0addb09158b4958175dc4"
+        },
+        {
+            "title": "Khu nhà tập thể Thiên Sơn Mộ Tuyết",
+            "address": "số 8 ngõ 95/58/12",
+            "subDistrict": "Tân Phú",
+            "district": "Quận 9",
+            "city": "Thành phố Hồ Chí Minh",
+            "rate": 5,
+            "statisticNumber": 2,
+            "posterId": "5fe1b10ecb06643a2488e8af",
+            "ratingId": "5fe1b10ecb06643a2488e8ae"
+        },
+        {
+            "title": "Biệt thự ven Hồ phường 3 quận Phú Nhuận",
+            "address": "số 8 ngõ 95/58/12",
+            "subDistrict": "Phúc Xá",
+            "district": "Ba Đình",
+            "city": "Hà Nội",
+            "rate": 0,
+            "statisticNumber": 1,
+            "posterId": "5fe0addb09158b4958175dc9",
+            "ratingId": "5fe0addb09158b4958175dc8"
+        },
+        {
+            "title": "Biệt thự Hòa Bình",
+            "address": "số 8 ngõ 10/9",
+            "subDistrict": "Hàng Bồ",
+            "district": "Hoàn Kiếm",
+            "city": "Hà Nội",
+            "rate": 0,
+            "statisticNumber": 1,
+            "posterId": "5fe1aae621551542fc12d452",
+            "ratingId": "5fe1aae621551542fc12d451"
+        }
+    ]
+
+    const ratePerMonth = [
+        {
+            "title": "Biệt thự ven Hồ phường 3 quận Phú Nhuận",
+            "address": "số 8 ngõ 95/58/12",
+            "subDistrict": "Phúc Xá",
+            "district": "Ba Đình",
+            "city": "Hà Nội",
+            "rate": 4.5,
+            "statisticNumber": 0,
+            "posterId": "5fe0ade609158b4958175dd1",
+            "ratingId": "5fe0ade609158b4958175dd0"
+        },
+        {
+            "title": "Biệt thự ven Hồ phường 3 quận Phú Nhuận",
+            "address": "số 8 ngõ 95/58/12",
+            "subDistrict": "Phúc Xá",
+            "district": "Ba Đình",
+            "city": "Hà Nội",
+            "rate": 0,
+            "statisticNumber": 0,
+            "posterId": "5fe0addb09158b4958175dc5",
+            "ratingId": "5fe0addb09158b4958175dc4"
+        },
+        {
+            "title": "Khu nhà tập thể Thiên Sơn Mộ Tuyết",
+            "address": "số 8 ngõ 95/58/12",
+            "subDistrict": "Tân Phú",
+            "district": "Quận 9",
+            "city": "Thành phố Hồ Chí Minh",
+            "rate": 5,
+            "statisticNumber": 0,
+            "posterId": "5fe1b10ecb06643a2488e8af",
+            "ratingId": "5fe1b10ecb06643a2488e8ae"
+        },
+        {
+            "title": "Biệt thự ven Hồ phường 3 quận Phú Nhuận",
+            "address": "số 8 ngõ 95/58/12",
+            "subDistrict": "Phúc Xá",
+            "district": "Ba Đình",
+            "city": "Hà Nội",
+            "rate": 0,
+            "statisticNumber": 0,
+            "posterId": "5fe0addb09158b4958175dc9",
+            "ratingId": "5fe0addb09158b4958175dc8"
+        },
+        {
+            "title": "Biệt thự Hòa Bình",
+            "address": "số 8 ngõ 10/9",
+            "subDistrict": "Hàng Bồ",
+            "district": "Hoàn Kiếm",
+            "city": "Hà Nội",
+            "rate": 0,
+            "statisticNumber": 0,
+            "posterId": "5fe1aae621551542fc12d452",
+            "ratingId": "5fe1aae621551542fc12d451"
+        }
+    ]
 
     const [postersList, setPosterList] = useState([])
 
@@ -93,7 +211,96 @@ function AdminStatisticTabs() {
 
     return (
         <div>
+            <div id="data-in-time">
+                <h1>Theo tuần</h1>
+                <div className="data-row">
+                    <div className="statistic-box-wrapper">
+                        <p className="statistic-title">Đánh giá</p>
+                        <div className="statistic-box">
+                            {ratePerWeek.map(value => {
+                                return (
+                                    <div className="data-row">
+                                        <Tooltip title={value.title}>
+                                            <div className="room-title">{value.title}</div>
+                                        </Tooltip>
+                                        <Statistic title="" value={value.rate}/>
+                                    </div>
+                                )
+                            })}
+                        </div>
+                    </div>
 
+                    <div className="statistic-box-wrapper">
+                        <p className="statistic-title">Lượt thích</p>
+                        <div className="statistic-box">
+                            {ratePerWeek.map(value => {
+                                return (
+                                    <div className="data-row">
+                                        <Statistic title="" value={value.rate}/>
+                                    </div>
+                                )
+                            })}
+                        </div>
+                    </div>
+
+                    <div className="statistic-box-wrapper">
+                        <p className="statistic-title">Lượt xem</p>
+                        <div className="statistic-box">
+                            {ratePerWeek.map(value => {
+                                return (
+                                    <div className="data-row">
+                                        <Statistic title="" value={value.rate}/>
+                                    </div>
+                                )
+                            })}
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div id="data-in-time">
+                <h1>Theo tháng</h1>
+                <div className="data-row">
+                    <div className="statistic-box-wrapper">
+                        <p className="statistic-title">Đánh giá</p>
+                        <div className="statistic-box">
+                            {ratePerMonth.map(value => {
+                                return (
+                                    <div className="data-row">
+                                        <Statistic title="" value={value.rate}/>
+                                    </div>
+                                )
+                            })}
+                        </div>
+                    </div>
+
+                    <div className="statistic-box-wrapper">
+                        <p className="statistic-title">Lượt thích</p>
+                        <div className="statistic-box">
+                            {ratePerMonth.map(value => {
+                                return (
+                                    <div className="data-row">
+                                        <Statistic title="" value={value.rate}/>
+                                    </div>
+                                )
+                            })}
+                        </div>
+                    </div>
+
+                    <div className="statistic-box-wrapper">
+                        <p className="statistic-title">Lượt xem</p>
+                        <div className="statistic-box">
+                            {ratePerMonth.map(value => {
+                                return (
+                                    <div className="data-row">
+                                        <Statistic title="" value={value.rate}/>
+                                    </div>
+                                )
+                            })}
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
